b437064e6433a087700e37bcd3ae6c3a3029ddde
Seyone Gunasingham <s.g.seyone@proton.me>
2025-09-16 18:33:21 +0530

    I really should've done proper VC on this

--- app/src/main/java/com/seyone22/shot/presentation/MainActivity.kt
+++ app/src/main/java/com/seyone22/shot/presentation/MainActivity.kt
@@ -9,23 +9,9 @@ import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.wear.compose.material.MaterialTheme
-import androidx.wear.compose.material.Text
-import androidx.wear.compose.material.TimeText
-import androidx.wear.tooling.preview.devices.WearDevices
-import com.seyone22.shot.R
-import com.seyone22.shot.presentation.screen.shotscreen.ShotScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.seyone22.shot.presentation.screen.shotscreenpe.ShotScreen
 import com.seyone22.shot.presentation.theme.ShotTheme
 
 class MainActivity : ComponentActivity() {
